921f82f4c417e46ec15daf6d6b563863b07b7ddf
dorel <dorelhayani@gmail.com>
2025-03-19 18:47:19 +0200

    building basic server

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const port = 3210;
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+const path = require('path');
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get("/",(req, res) =>  {
-    res.send("Good Morning");
+    res.send("Good evening");
 });
+
+
+app.listen(port, ()=> { console.log(`Now Listening On Port http://localhost:${port}`); })
