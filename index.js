1f42a9e5890d187ecefd37cfddd6ad5b4d489bb6
dorel <dorelhayani@gmail.com>
2025-05-28 17:48:58 +0300

    adding login page , containing authentication via username and password.
    adding encryption via md5 and jsonwebtoken for cookies.

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-// slashes@2.0.0
+// Installed Libraries
+// slashes@2.0.0 , cookie-parser , jwt = jsonwebtoken
 const port = 3210;
 const express = require('express');
 const app = express();
 app.use(express.json());
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-app.set("view engine", "ejs");
-
 const path = require('path');
-app.set('views', path.join(__dirname, "./views") )
-app.use(express.static(path.join(__dirname, "css")));
-app.use(express.static(path.join(__dirname, "js")));
+app.use(bodyParser.urlencoded({extended: false}));
+
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+global.jwt = require('jsonwebtoken');
+
+let db_M = require('./server/models/database');
+global.db_pool = db_M.pool;
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, "./views"));
 
 global.addSlashes = require('slashes').addSlashes;
 global.stripSlashes = require('slashes').stripSlashes;
 
-let db_M = require('./server/models/database');
-global.db_pool = db_M.pool;
-app.get('/index', (req, res)=>{ res.render("index",{}); })
+global.was_logged = false;
 
-const login_rtr = require('./server/Routs/login_R')
-app.use('/', login_rtr);
+const users_MID = require("./server/middleware/Users_mid");
 
-const corse_rtr = require('./server/Routs/Course_R')
-app.use('/course', corse_rtr);
+const corse_rtr = require('./server/Routs/Course_R');
+app.use('/course', [users_MID.isLogged],corse_rtr);
 
-const users_rtr = require('./server/Routs/Users_R')
-app.use('/users', users_rtr);
-app.listen(port, ()=> { console.log(`Now Listening On Port http://localhost:${port}`); })
+const users_rtr = require('./server/Routs/Users_R');
+app.use('/users', [users_MID.isLogged], users_rtr);
+
+const auth_R = require('./server/Routs/auth_R');
+app.use('/', auth_R);
+app.get('/', (req, res)=>{ res.render("index",{}); });
+app.listen(port, ()=> { console.log(`Now Listening On Port http://localhost:${port}`); });
--- server/middleware/Users_mid.js
+++ server/middleware/Users_mid.js
@@ -1,29 +1,72 @@
 var md5 = require('md5');
-async function CheckLogin(userName, password){
-    let enc_poas = md5("A" + password);
-    let Query = `SELECT * FROM users userName = ${userName} AND password = ${enc_poas}`;
+
+// is Logged Function
+// =====================================================================================================================
+async function isLogged(req, res, next){
+    const jwtToken = req.cookies.ImLoggedToYoman;
+    let user_id=-1;
+    if (jwtToken !== "") {
+        jwt.verify(jwtToken, 'myPrivateKey', async (err, decodedToken) => {
+            console.log("decodedToken=",decodedToken);
+            if (err) { console.log("err=",err); }
+            else {
+                let data = decodedToken.data;
+                console.log("data=",data);
+                user_id = data.split(",")[0];
+            }
+        })
+    }
+    if(user_id < 0) res.redirect("/login");
+    next();
+}
+// =====================================================================================================================
+
+
+// Check Login Function
+// =====================================================================================================================
+async function CheckLogin(req, res, next){
+    let userName = (req.body.userName !== undefined) ? addSlashes(req.body.userName) : "";
+    let password = (req.body.password !== undefined) ? req.body.password : "";
+    let enc_pass = md5("A"+password);
+    let Query = `SELECT * FROM users WHERE userName = '${userName}' AND password = '${enc_pass}'`;
 
     const promisePool = db_pool.promise();
     let rows=[];
     try { [rows] = await promisePool.query(Query); }
-    catch (err) { console.log(err);}
-    return (rows.length > 0);
+    catch (err) { console.log(err); }
+
+    if(rows.length > 0){
+        req.validUser = true;
+        let val = `${rows[0].id},${rows[0].name}`;
+        var token = jwt.sign(
+            {data: val},
+            'myPrivateKey',
+            { expiresIn: 31*24*60*60 }); // in sec
+        res.cookie("ImLoggedToYoman", token, { maxAge: 31*24*60*60 * 1000, }); } // 3hrs in ms
+    console.log("Generated token:", token);
+    console.log("Cookies in response:", res.getHeader("Set-Cookie"));
+    next();
 }
+// =====================================================================================================================
+
+
 // Create
 // =====================================================================================================================
 async function AddUser(req, res, next){
     let name = (req.body.name !== undefined) ? addSlashes(req.body.name): "";
     let userName = (req.body.userName !== undefined)  ?addSlashes(req.body.userName): "";
-    let password = (req.body.password !== undefined) ? md5(req.body.password): "";
-    let enc_poas = md5("A" * password);
+    let password = (req.body.password !== undefined) ? req.body.password: "";
+    let enc_pass = md5("A"+password);
     let email = (req.body.email !== undefined) ? addSlashes(req.body.email): "";
-    let typeID = (req.body.typeID !== undefined) ? parseInt(req.body.typeID): -1;
+    let typeID = (req.body.typeID !== undefined) ? Number(req.body.typeID): -1;
     let StudentID = (req.body.StudentID !== undefined) ? addSlashes(req.body.StudentID): "";
 
-    let Query=`INSERT INTO users (name,userName,password,email,typeID,StudentID) 
-    VALUES ('${name}','${userName}','${password}','${email}','${typeID}','${StudentID}')` ;
-    const promisePool = db_pool.promise();
+    let Query="INSERT INTO users";
+    Query +="(`name`,`userName`,`password`,`email`,`typeID`,`StudentID`)";
+    Query +="VALUES";
+    Query +=`('${name}','${userName}','${enc_pass}','${email}','${typeID}','${StudentID}')`;
 
+    const promisePool = db_pool.promise();
     let rows = [];
     try{ [rows] = await promisePool.query(Query); }
     catch (err){ console.log(err) }
@@ -44,7 +87,6 @@ async function UpdateUser(req, res, next){
 
     let name = (req.body.name !== undefined) ? addSlashes(req.body.name): "";
     let userName = (req.body.userName !== undefined)  ?addSlashes(req.body.userName): "";
-    // let password = (req.body.password !== undefined) ? addSlashes(req.body.password): "";
     let email = (req.body.email !== undefined) ? addSlashes(req.body.email): "";
     let typeID = (req.body.typeID !== undefined) ? parseInt(req.body.typeID): -1;
     let StudentID = (req.body.StudentID !== undefined) ? addSlashes(req.body.StudentID): "";
@@ -53,7 +95,6 @@ async function UpdateUser(req, res, next){
     let Query =`UPDATE users SET `;
     Query +=`name   ='${name}' ,`;
     Query +=`userName  ='${userName}' ,`;
-    // Query +=`password ='${password}' ,`;
     Query +=`email  ='${email}' ,`;
     Query +=`typeID='${typeID}' ,`;
     Query +=`StudentID     ='${StudentID}'  `;
@@ -122,4 +163,4 @@ async function DeleteUser(req,res,next){
 }
 // =====================================================================================================================
 
-module.exports = { AddUser,GetOneUser,GetAllUsers,UpdateUser,DeleteUser, CheckLogin }
+module.exports = { AddUser,GetOneUser,GetAllUsers,UpdateUser,DeleteUser, CheckLogin, isLogged }
